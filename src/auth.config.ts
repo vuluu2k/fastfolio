5e99b99da47cb732afe6d85331c75c670207a427
vuluu <vuluu040320@gmail.com>
2025-09-23 10:38:43 +0700

    add prisma adapter

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -1,15 +1,32 @@
 import Google from "next-auth/providers/google";
 import GitHub from "next-auth/providers/github";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import prisma from "@/lib/prisma";
 
 export const authConfig = {
+  adapter: PrismaAdapter(prisma),
   providers: [
     Google({
       clientId: String(process.env.GOOGLE_CLIENT_ID ?? ""),
       clientSecret: String(process.env.GOOGLE_CLIENT_SECRET ?? ""),
+      authorization: {
+        params: {
+          // Ask Google for a refresh_token
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
     }),
     GitHub({
       clientId: String(process.env.GITHUB_ID ?? ""),
       clientSecret: String(process.env.GITHUB_SECRET ?? ""),
+      authorization: {
+        params: {
+          // Request standard scopes; offline_access may require org settings enabled
+          scope: "read:user user:email offline_access",
+        },
+      },
     }),
   ],
   pages: {
